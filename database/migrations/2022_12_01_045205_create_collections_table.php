c56b8699c4d45b6f9fda1231d955bd19e0e479f5
Bayan <itebayan@gmail.com>
2022-12-04 16:57:29 +0200

    dd

--- database/migrations/2022_12_01_045205_create_collections_table.php
+++ database/migrations/2022_12_01_045205_create_collections_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
         Schema::create('collections', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->int('status');
+            $table->Integer('status');
             $table->timestamps();
         });
     }
